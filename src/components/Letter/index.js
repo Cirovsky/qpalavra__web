8f2eeb72a6e84b425dc3663d2ef539aab57c50e6
Cirovsky <cirovisqui@gmail.com>
2023-03-20 20:03:46 -0300

    criado highlight quando o "chute pode ser enviado

--- src/components/Letter/index.js
+++ src/components/Letter/index.js
@@ -11,13 +11,13 @@ const Letter = (props) => {
     }
 
     return (
-        props.line == 0 && props.index == 0? 
+        props.line === 0 && props.index === 0? 
         <li className='letter__li'>
-            <p className={`letter ${props.style} ${actualLine==props.line? 'line_selected': ''}`}>{props.letter}</p>
+            <p className={`letter ${props.class? props.class : ""} ${props.style} ${actualLine==props.line? 'line_selected': ''}`}>{props.letter}</p>
             <div id={'letter'+ props.line + props.index} className='underline selected'></div>
         </li>:
         <li className='letter__li'>
-            <p className={`letter ${props.style} ${actualLine==props.line? 'line_selected': ''}`}>{props.letter}</p>
+            <p className={`letter ${props.class? props.class : ""} ${props.style} ${actualLine==props.line? 'line_selected': ''}`}>{props.letter}</p>
             <div id={'letter'+ props.line + props.index} className='underline'></div>
         </li>
     )
--- src/components/Line/index.jsx
+++ src/components/Line/index.jsx
@@ -1,18 +1,26 @@
 import Letter from '../Letter';
 import './Line.css';
 
+
 const Line = (props) =>{
-    
-    return (
-        <ul className={`line`} >
+    const highlight = props.line.indexOf('') == -1;
+    return (highlight ?
+        <ul className={'line'} id={`line${props.l}`}>
+            <Letter letter = {props.line[0]} index={0} line={props.l} style={props.style[0]} class={'highlight'}/>
+            <Letter letter = {props.line[1]} index={1} line={props.l} style={props.style[1]} class={'highlight'}/>
+            <Letter letter = {props.line[2]} index={2} line={props.l} style={props.style[2]} class={'highlight'}/>
+            <Letter letter = {props.line[3]} index={3} line={props.l} style={props.style[3]} class={'highlight'}/>
+            <Letter letter = {props.line[4]} index={4} line={props.l} style={props.style[4]} class={'highlight'}/>
+        </ul>
+        :
+        <ul className={'line'} id={`line${props.l}`} >
             <Letter letter = {props.line[0]} index={0} line={props.l} style={props.style[0]}/>
             <Letter letter = {props.line[1]} index={1} line={props.l} style={props.style[1]}/>
             <Letter letter = {props.line[2]} index={2} line={props.l} style={props.style[2]}/>
             <Letter letter = {props.line[3]} index={3} line={props.l} style={props.style[3]}/>
             <Letter letter = {props.line[4]} index={4} line={props.l} style={props.style[4]}/>
         </ul>
-        
-    )
+    );
 }
 
 export default Line;
